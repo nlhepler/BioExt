e2379208e2c3f0d97346afd09537ed1ccc590818
Lance Hepler <nlhepler@gmail.com>
2012-08-14 17:55:00 -0700

    add a bunch of nl4-3 genes

--- lib/BioExt/_references/__init__.py
+++ lib/BioExt/_references/__init__.py
@@ -10,4 +10,4 @@ __all__ = ['hxb2', 'nl4_3']
 # annoyingly, glob doesn't support curly bracket {fa,gb}-style wildcards
 # so use this bracket-hack
 hxb2 = _reffactory(join(_refdir, 'hxb2'), 'HXB2_%s.[fg][ab]')
-nl4_3 = _reffactory(join(_refdir, 'nl4-3'), 'NL4_3_%s.fa')
+nl4_3 = _reffactory(join(_refdir, 'nl4-3'), 'NL4_3_%s.fna')
